2a7203d03c894287032720cbc695b957a6e693ac
kimdh-hyw <71587564+kimdh-hyw@users.noreply.github.com>
2021-04-27 20:17:05 +0900

    modified source

--- src/sec01_inheritancepolymorphism/EX01_Inheritance/Inheritance.java
+++ src/sec01_inheritancepolymorphism/EX01_Inheritance/Inheritance.java
@@ -20,15 +20,15 @@ public class Inheritance {
 	public static void main(String[] args) {
 		//#1. Human 객체 생성
 		Human h = new Human();
-		h.name = "김민성";
-		h.age = 15;
+		h.name = "김현지";
+		h.age = 11;
 		h.eat();
 		h.sleep();
 		
 		//#2. Student 객체 생성
 		Student s = new Student();
 		s.name = "김민성";
-		s.age = 15;
+		s.age = 16;
 		s.studentID =128;
 		s.eat();
 		s.sleep();
@@ -36,31 +36,13 @@ public class Inheritance {
 		
 		//#3. Worker 객체 생성
 		Worker w = new Worker();
-		w.name = "김현지";
-		w.age = 30;
+		w.name = "봉윤정";
+		w.age = 45;
 		w.workerID = 128;
 		w.eat();
 		w.sleep();
 		w.goToWork();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
